df13747ae501d8f691aac75e4e4e05272e05dfac
ichater <chaterizaak@gmail.com>
2021-04-01 17:21:54 +1100

    Queen takes queen validator

--- src/exercises/QueenAttack.ts
+++ src/exercises/QueenAttack.ts
@@ -1,29 +1,57 @@
 // Given the position of two queens on a chess board,
 // indicate whether or not they are positioned so that they can attack each other.
 
-const createChessBoard = (): number[][] =>
-  [...Array(8)].map(() => [...Array(8)].map((_, i) => 1));
-
-const chessBoard = createChessBoard();
-
-interface Position {
+interface Board {
   x: number;
   y: number;
 }
 
-const queenTakesQueen = (blackQueen: Position, whiteQueen: Position): boolean =>
-  false;
+type Position = Board;
+
+type GameBoard = number[][];
+
+const standardChessBoard: Board = { x: 8, y: 8 };
+
+const createChessBoard = (board: Board): number[][] =>
+  [...Array(board.x)].map(() => [...Array(board.y)].map((_, i) => 1));
+
+const chessBoard: GameBoard = createChessBoard(standardChessBoard);
+
+const queenTakesQueen = (
+  blackQueen: Position,
+  whiteQueen: Position,
+  chessBoard: GameBoard
+): boolean =>
+  dualQueenPositionValidator(blackQueen, whiteQueen, chessBoard) &&
+  (queenTakesQueenCastle(blackQueen, whiteQueen) ||
+    queenTakesQueenVerticle(blackQueen, whiteQueen));
+
+const queenTakesQueenCastle = (
+  blackQueen: Position,
+  whiteQueen: Position
+): boolean => blackQueen.x === whiteQueen.x || blackQueen.y === whiteQueen.y;
+
+const queenTakesQueenVerticle = (
+  blackQueen: Position,
+  whiteQueen: Position
+): boolean =>
+  Math.abs(blackQueen.x - whiteQueen.x) ===
+  Math.abs(blackQueen.y - whiteQueen.y);
 
 const dualQueenPositionValidator = (
   blackQueen: Position,
   whiteQueen: Position,
-  board
+  board: GameBoard
 ): boolean =>
-  isValidPosition(blackQueen, board) &&
-  isValidPosition(whiteQueen, board) &&
-  blackQueen !== whiteQueen;
+  isValidPosition(blackQueen, board) && isValidPosition(whiteQueen, board);
 
-const isValidPosition = ({ x, y }: Position, board): boolean =>
+const queensDifferentPositionValidator = (
+  blackQueen: Position,
+  whiteQueen: Position
+): boolean =>
+  blackQueen.x === whiteQueen.x && blackQueen.y === whiteQueen.y ? false : true;
+
+const isValidPosition = ({ x, y }: Position, board: GameBoard): boolean =>
   x <= board[0].length && y <= board.length && x > 0 && y > 0;
 
 export {
@@ -31,4 +59,5 @@ export {
   isValidPosition,
   chessBoard,
   dualQueenPositionValidator,
+  queensDifferentPositionValidator,
 };
--- src/tests/QueenAttack.test.ts
+++ src/tests/QueenAttack.test.ts
@@ -3,11 +3,30 @@ import {
   isValidPosition,
   chessBoard,
   dualQueenPositionValidator,
+  queensDifferentPositionValidator,
 } from "../exercises/QueenAttack";
 
-// describe("Can one queen take another", () => {
-//   it("queen castle takes queen on x axis"), () => {};
-// });
+describe("Can one queen take another", () => {
+  it("queen takes queen Castle", () => {
+    expect(
+      queenTakesQueen({ x: 1, y: 3 }, { x: 2, y: 3 }, chessBoard)
+    ).toBeTruthy();
+    expect(
+      queenTakesQueen({ x: 1, y: 3 }, { x: 1, y: 5 }, chessBoard)
+    ).toBeTruthy();
+    expect(
+      queenTakesQueen({ x: 1, y: 5 }, { x: 2, y: 3 }, chessBoard)
+    ).toBeFalsy();
+  });
+  it("queen takes queen Bishop", () => {
+    expect(
+      queenTakesQueen({ x: 1, y: 3 }, { x: 2, y: 3 }, chessBoard)
+    ).toBeTruthy();
+    expect(
+      queenTakesQueen({ x: 1, y: 5 }, { x: 2, y: 3 }, chessBoard)
+    ).toBeFalsy();
+  });
+});
 
 describe("queen game helper functions", () => {
   it("queens position is valid", () => {
@@ -26,7 +45,12 @@ describe("queen game helper functions", () => {
   });
   it("two queens cant be in the exact same position on the table", () => {
     expect(
-      dualQueenPositionValidator({ x: 1, y: 3 }, { x: 1, y: 3 }, chessBoard)
+      queensDifferentPositionValidator({ x: 1, y: 3 }, { x: 1, y: 3 })
     ).toBeFalsy();
+  });
+  it("Two queens, two different positions validated", () => {
+    expect(
+      queensDifferentPositionValidator({ x: 2, y: 3 }, { x: 7, y: 3 })
+    ).toBeTruthy();
   });
 });
